92e442930bd5013859d686893d0ac49945fb0c03
anshita066 <anshita_066@yahoo.in>
2021-11-03 08:46:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.livedata
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity :AppCompatActivity() {
+
+    //    lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: TestViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(TestViewModel::class.java)
+
+        colourbutton.setOnClickListener {
+            colourFragmentLayout.setBackgroundColor(viewModel.colour)
+            viewModel.changeBackgroundColor()
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Getting last updated value from viewModel
+        // So that in case of configuration change,last updated value remains with user
+        colourFragmentLayout.setBackgroundColor(viewModel.colour)
+    }
+}
---
+++ app/src/main/java/com/example/livedata/TestViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.livedata
+
+import android.graphics.Color
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class TestViewModel: ViewModel() {
+
+
+    private val colorArray = arrayOf(
+        Color.BLACK,
+        Color.BLUE,
+        Color.DKGRAY,
+        Color.GRAY,
+        Color.GREEN,
+        Color.MAGENTA
+    )
+
+    var colour = colorArray[0]
+
+
+    //Setting Background
+    fun changeBackgroundColor() {
+        colour = colorArray[randomNumberGenerator()]
+    }
+
+    // Generate random number
+    private fun randomNumberGenerator(): Int {
+        val startRange = 0
+        val endRange = colorArray.size
+        return Random.nextInt(startRange, endRange)
+    }
+}
